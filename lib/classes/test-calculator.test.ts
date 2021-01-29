5e369c1dfcdc5feb445d589a250aef6716572191
CodingVampyre <tobiaskavsek@hotmail.de>
2021-01-30 00:22:52 +0100

    :wrench: write small test file

--- lib/classes/test-calculator.test.ts
+++ lib/classes/test-calculator.test.ts
@@ -15,4 +15,29 @@ describe('tests', () => {
         expect(results).toStrictEqual([1, 0]);
     });
 
+    test('should calculate a more complex test', () => {
+        
+        const results = TestCalculator.CalculateTestResults([
+            {
+                answer: 1,
+                weights: [1, 0],
+            },
+            {
+                answer: 0.5,
+                weights: [0, 1],
+            },
+            {
+                answer: -1,
+                weights: [0.5, -1],
+            },
+            {
+                answer: -0.5,
+                weights: [-1, 1],
+            }
+        ]);
+
+        expect(results).toStrictEqual([0.4, 0.3333333333333333]);
+
+    });
+
 });
